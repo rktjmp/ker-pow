a0dcf18d9fb9c9ded8696ae301ddf8b4ddfb13bc
RKTJMP <spam _ criticaleffect net>
2012-06-01 05:35:47 +1000

    Won't show a link default.dev if that symlink is present

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 get '/' do
   
   links = Dir.entries('../').select do |entry|
-    File.symlink? File.join("..", entry)
+    File.symlink? File.join("..", entry) and entry != "default"
   end.map { |symlink| "http://#{symlink}.dev" }
 
   erb :index, locals: {links: links}
