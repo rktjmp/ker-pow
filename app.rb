7bdcde013bebe14342b668936d84b7824505298b
RKTJMP <spam _ criticaleffect net>
2012-06-01 14:42:42 +1000

    Added restart app link

--- app.rb
+++ app.rb
@@ -3,10 +3,15 @@ require 'sinatra'
 
 get '/' do
   
-  links = Dir.entries('../').select do |entry|
+  apps = Dir.entries('../').select do |entry|
     File.symlink? File.join("..", entry) and entry != "default"
-  end.map { |symlink| "http://#{symlink}.dev" }
+  end.map {|symlink| {name: symlink, link: "http://#{symlink}.dev"}}
 
-  erb :index, locals: {links: links}
+  erb :index, locals: {apps: apps}
 
+end
+
+get '/restart/:symlink' do
+	FileUtils.touch("../#{params[:symlink]}/tmp/restart.txt")
+	redirect to("http://#{params[:symlink]}.dev")
 end
