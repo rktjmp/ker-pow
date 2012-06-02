010ef6debdbb2140726121a889ce5c3fed148df2
RKTJMP <spam _ criticaleffect net>
2012-06-02 17:10:19 +1000

    Added basic log viewer.

--- app.rb
+++ app.rb
@@ -20,3 +20,15 @@ get '/restart/:symlink' do
 		erb :restart_error, locals: {app: app}
 	end
 end
+
+get '/log/:symlink' do 
+	log_path = "../#{params[:symlink]}/log/development.log"
+	if File.exists?(log_path)
+		log_lines = `tail -n 50 #{log_path}`.lines
+		@page_classes = "log"
+		erb :log, locals: {log_lines: log_lines}
+	else
+		app = {name: params[:symlink], link: "http://#{params[:symlink]}.dev"}
+		erb :log_error, locals: {app: app}
+	end
+end
